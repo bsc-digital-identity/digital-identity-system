cd3b91162828dd4bb029cc6b8756437f362ecfc9
kawixKrul <macioukos@gmail.com>
2025-07-09 11:40:06 +0200

    build working

--- api/main.go
+++ api/main.go
@@ -2,8 +2,16 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"net/http"
 )
 
+func handler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "Hello from Go Docker multistage")
+}
+
 func main() {
-	fmt.Println("Hello api!")
+	http.HandleFunc("/", handler)
+	fmt.Println("server running at 0.0.0.0:8080")
+	log.Fatal(http.ListenAndServe("0.0.0.0:8080", nil))
 }
