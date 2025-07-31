1b46acedf71eeeab2e60113663525fdaef1e8fe7
kawixKrul <macioukos@gmail.com>
2025-07-31 18:47:08 +0200

    post refactor path fix

--- dev_tools/scripts/smart_contract.sh
+++ dev_tools/scripts/smart_contract.sh
@@ -1,7 +1,16 @@
 #!/bin/bash
 
+# resolve directories
+PROJECT_ROOT=$(cd "$(dirname "${BASH_SOURCE[0]}")/../../" && pwd -P)
+
+SMART_CONTRACT_DIR="${PROJECT_ROOT}/system/smart-contract"
+BLOCKCHAIN_CLIENT_DIR="${PROJECT_ROOT}/system/blockchain-client"
+CLIENT_DIR="${SMART_CONTRACT_DIR}/client"
+PROGRAM_DIR="${SMART_CONTRACT_DIR}/program"
+DIST_DIR="${SMART_CONTRACT_DIR}/dist"
+
 build_sbf() {
-    cargo build-sbf --manifest-path=program/Cargo.toml --sbf-out-dir=dist/program
+    cargo build-sbf --manifest-path="${PROGRAM_DIR}/Cargo.toml" --sbf-out-dir="${DIST_DIR}/program"
 }
 
 check_toolchain() {
@@ -27,7 +36,7 @@ cleanup() {
         echo "Stopping process on pid $pid"
         kill $pid
     done
-    echo "All background processess stopped"
+    echo "All background processes stopped"
     exit 0
 }
 
@@ -44,16 +53,16 @@ case $1 in
         echo "solana-test-validator must be started before running this command or it will fail"
         build_sbf
         solana config set --url http://127.0.0.1:8899
-        solana program deploy dist/program/identity_app.so --program-id dist/program/identity_app-keypair.json
-        cp dist/program/identity_app-keypair.json ../blockchain-client/
+        solana program deploy "${DIST_DIR}/program/identity_app.so" --program-id "${DIST_DIR}/program/identity_app-keypair.json"
+        cp "${DIST_DIR}/program/identity_app-keypair.json" "${BLOCKCHAIN_CLIENT_DIR}/"
         ;;
     "client")
-        (cd client/; cargo run /dist/program/helloworld-keypair.json)
+        (cd "${CLIENT_DIR}" && cargo run "${DIST_DIR}/program/identity_app-keypair.json")
         ;;
     "clean")
-        (cd program/; cargo clean)
-        (cd client/; cargo clean)
-        rm -rf dist/
+        (cd "${PROGRAM_DIR}" && cargo clean)
+        (cd "${CLIENT_DIR}" && cargo clean)
+        rm -rf "${DIST_DIR}"
         ;;
     *)
         echo "usage: $0 [check|build|deploy|client|clean]"
