06553c5e9a84c4a1084c0f38fc9b8cf55859bf83
kawixKrul <macioukos@gmail.com>
2025-08-07 11:41:20 +0200

    test runner improvment

--- dev_tools/scripts/test_runner.sh
+++ dev_tools/scripts/test_runner.sh
@@ -10,19 +10,29 @@ PKG_TEST_DIR="${PROJECT_ROOT}/system/pkg/${TEST_DIR_NAME}"
 
 run_all() {
     cd $BLOCKCHAIN_CLIENT_TEST_DIR/..
+    go clean -cache && go clean -testcache
     go test -v $BLOCKCHAIN_CLIENT_TEST_DIR
+    
     cd $API_TEST_DIR/..
+    go clean -cache && go clean -testcache
     go test -v $API_TEST_DIR
+    
     cd $PKG_TEST_DIR/..
+    go clean -cache && go clean -testcache
     go test -v $PKG_TEST_DIR
 }
 
 becnmark_all() {
     cd $BLOCKCHAIN_CLIENT_TEST_DIR/..
+    go clean -cache && go clean -testcache
     go test -bench . -run notest $BLOCKCHAIN_CLIENT_TEST_DIR
+    
     cd $API_TEST_DIR/..
+    go clean -cache && go clean -testcache
     go test -bench . -run notest $API_TEST_DIR
+    
     cd $PKG_TEST_DIR/..
+    go clean -cache && go clean -testcache
     go test -bench . -run notest $PKG_TEST_DIR
 }
 
@@ -34,6 +44,6 @@ case $1 in
         becnmark_all
     ;;
     *)
-        echo "usage: $0 [all|bench]"
+        echo "usage: $0 [test|bench]"
         ;;
 esac
