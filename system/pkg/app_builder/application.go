7173c1dcda58a1269fe6b91bc13e56f43672c6e3
kawixKrul <macioukos@gmail.com>
2025-08-25 17:28:22 +0200

    connection shutdowns when app closes

--- system/pkg/app_builder/application.go
+++ system/pkg/app_builder/application.go
@@ -1,8 +1,14 @@
 package appbuilder
 
 import (
+	"context"
+	"net/http"
+	"os"
+	"os/signal"
 	"pkg-common/logger"
 	"pkg-common/rabbitmq"
+	"syscall"
+	"time"
 
 	"github.com/gin-gonic/gin"
 	amqp "github.com/rabbitmq/amqp091-go"
@@ -28,6 +34,36 @@ func (a *Application) Start() {
 		go ws.StartService()
 	}
 
-	a.Logger.Infof("REST API is now listening on: %s", a.Addr)
-	a.Engine.Run(a.Addr)
+	a.Logger.Info("Starting server...")
+	srv := &http.Server{
+		Addr:    a.Addr,
+		Handler: a.Engine,
+	}
+
+	go func() {
+		a.Logger.Infof("REST API is now listening on: %s", a.Addr)
+		if err := srv.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			a.Logger.Fatalf(err, "Listen errors: ")
+		}
+	}()
+
+	exit := make(chan os.Signal, 1)
+	signal.Notify(exit, syscall.SIGTERM, syscall.SIGINT)
+	<-exit
+
+	a.Logger.Info("Recevied shutdown signal, closing application...")
+
+	ctx, cancel := context.WithTimeout(context.Background(), 15*time.Second)
+	defer cancel()
+
+	if err := srv.Shutdown(ctx); err != nil {
+		a.Logger.Errorf(err, "Server forced to shutdown.")
+	}
+
+	if a.Conn != nil {
+		a.Conn.Close()
+		a.Logger.Info("Connection to Rabbitmq server closed.")
+	}
+
+	a.Logger.Info("Application closed gracefully.")
 }
